f6c12dceb60237a412d4c596fc78e71f36d5ac23
zhourui <1623040547@qq.com>
2024-08-08 16:46:08 +0800

    节点遍历最简单实现

---
+++ lib/tester.dart
@@ -0,0 +1,116 @@
+import 'dart:io';
+
+import 'package:analyzer/dart/analysis/utilities.dart';
+import 'package:analyzer_query/full_visitor.dart';
+import 'package:analyzer_query/mini/log.dart';
+import 'package:analyzer/src/dart/ast/ast.dart';
+
+class TestFile {
+  final String _dartString;
+
+  final Function(AstNodeImpl)? _visit;
+
+  late final List<TestToken> tokens;
+
+  TestFile._(this._dartString, {dynamic Function(AstNodeImpl)? visit})
+      : _visit = visit {
+    tokens = _testString(_dartString);
+  }
+
+  static TestFile fromString(
+    String dartString, {
+    Function(AstNodeImpl)? visit,
+  }) {
+    return TestFile._(
+      dartString,
+      visit: visit,
+    );
+  }
+
+  static TestFile fromFile(
+    String filePath, {
+    Function(AstNodeImpl)? visit,
+  }) {
+    return TestFile._(
+      File(filePath).readAsStringSync(),
+      visit: visit,
+    );
+  }
+
+  List<TestToken> _testString(String dartString) {
+    Map<String, TestToken> tokens = {};
+    final result = parseString(content: dartString);
+    result.unit.visitChildren(
+      FullVisitor(
+        (node) {
+          final token = TestToken(
+            start: node.offset,
+            end: node.end,
+            name: dartString.substring(node.offset, node.end),
+            type: node.runtimeType,
+          );
+          String? id = node.parent?.id;
+          if (tokens.containsKey(id)) {
+            tokens[id]?._isLeaf = false;
+          }
+          tokens[token.id] = token;
+          _visit?.call(node);
+        },
+      ),
+    );
+    return tokens.values.toList();
+  }
+
+  String showLeafNodeText() {
+    String text = _dartString;
+    for (var token in tokens.reversed) {
+      if (!token.isLeaf) continue;
+      text = text.replaceRange(token.start, token.end, token.nodeType);
+    }
+    analyzerLog('\n$text');
+    return text;
+  }
+
+  void showLeafNodeDict() {
+    for (var token in tokens) {
+      if (!token.isLeaf) continue;
+      analyzerLog("\n"
+          "type: ${token.nodeType}\n"
+          "name: ${token.name}\n");
+    }
+  }
+
+  void showNodeDict() {
+    for (var token in tokens) {
+      analyzerLog("\n"
+          "type: ${token.nodeType}\n"
+          "name: ${token.name}\n");
+    }
+  }
+}
+
+class TestToken {
+  final int start;
+  final int end;
+  final String name;
+  final Type type;
+  late final String id = '${type}_${start}_$end';
+
+  bool _isLeaf = true;
+
+  bool get isLeaf => _isLeaf;
+
+  String get nodeType =>
+      type.toString().substring(0, type.toString().length - 4);
+
+  TestToken({
+    required this.start,
+    required this.end,
+    required this.name,
+    required this.type,
+  });
+}
+
+extension on AstNode {
+  String get id => '${runtimeType}_${offset}_$end';
+}
